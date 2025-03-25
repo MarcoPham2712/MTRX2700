b952598cd0ea939c878f38e0a8e9be00fb96960f
Gluecat <160551218+Gluecat@users.noreply.github.com>
2025-03-25 22:49:30 +1100

    Update hardware_delay_4c.s

--- exercise_4/Src/hardware_delay_4c.s
+++ exercise_4/Src/hardware_delay_4c.s
@@ -14,13 +14,13 @@ main:
 
 @ To determine the appropriate prescaler value for setting the counter period
 	LDR R0, =TIM2	              	@ Load the base address for the timer
-	MOV R1, #0x07                 	@ Setting and putting a prescaler value 799 in R1
+	MOV R1, #0x07                 	@ Setting and putting a prescaler value 7 in R1
 	STR R1, [R0, TIM_PSC]         	@ Set the prescaler register
 
 @ Setting the trigger_prescaler function
 	BL trigger_prescaler
 
-@ Setting the Auto-reload preload enable
+@ Main logic function
     BL enable_arpe
     BL delayfunction
     BL LED_on
@@ -29,28 +29,28 @@ main:
 
 @ Set the LEDon function
 LED_on:
-    LDR R4, =0b11111111           	@ Turn on all LEDs
+    LDR R4, =0b11111111           	@ Load bitmask value into R4 and turn on all LEDs
     LDR R0, =GPIOE                	@ Load the address of the GPIOE register into R0
     STR R4, [R0, #ODR + 1]        	@ Store this to the second byte of the ODR (bits 8-15)
-    BL delayfunction              	@ Call back to the delayfunction
-	B LED_off                      	@ Go to the LEDoff function
+    BL delayfunction              	@ Return to the delayfunction
+	B LED_off
 
 @ Set the LEDoff function
 LED_off:
-    LDR R4, =0b00000000           	@ Turn off all LEDs
+    LDR R4, =0b00000000           	@ Load bitmask value into R4 and turn off all LEDs
     LDR R0, =GPIOE                	@ Load the address of the GPIOE register into R0
     STR R4, [R0, #ODR + 1]        	@ Store this to the second byte of the ODR (bits 8-15)
-    BL delayfunction              	@ Call back to the delayfunction
-	B LED_on                       	@ Go to the LEDon function
+    BL delayfunction              	@ Return to the delayfunction
+	B LED_on
 
 @ Delay equation
 delayfunction:
-    LDR R0, =TIM2                 	@ Loads the base address of the timer to R0
+    LDR R0, =TIM2                 	@ Loads the base address of the Timer2 to R0
 wait_loop:
-    LDR R1, [R0, #TIM_SR]         	@ Load Update Interrupt Flag (UIF) from status register
-    TST R1, #1                    	@ Tests if UIF is set
-    BEQ wait_loop                 	@ If UIF is not set, continue to wait
-    MOV R2, #0                    	@ Ready to clear the value of the UIF flag
-    STR R2, [R0, #TIM_SR]         	@ Clean the UIF flag
-    BX LR                         	@ Return from the function
+    LDR R1, [R0, #TIM_SR]         	@ Load the Timer 2 Status Register (TIM_SR) into R1
+    TST R1, #1                    	@ Test if the Update Interrupt Flag (UIF) is set
+    BEQ wait_loop                 	@ Continue to wait
+    MOV R2, #0                    	@ Clear the value of the UIF flag
+    STR R2, [R0, #TIM_SR]
+    BX LR
 
